085485bbc1fbe746b7b79b488c900cf10f2ff843
EndlessGG_ <johnsonsantizo26042003@gmail.com>
2022-12-01 16:51:05 -0600

    st hola
    string diciendo hola

--- Juegos/Program.cs
+++ Juegos/Program.cs
@@ -17,6 +17,8 @@ namespace Juegos
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            string hola = "hola";
         }
+
     }
 }
